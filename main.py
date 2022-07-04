9bf17ea0aabe8141b5f1766964afdb36eaa4c948
Ramon K. Shin <51011193+ramer-dev@users.noreply.github.com>
2022-07-04 14:58:42 +0900

    Bug Fixed

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-import cmath
-
 import cv2
 import mediapipe as mp
-import argparse
 import numpy as np
 from utils_display import DisplayHand
 from utils_mediapipe import MediaPipeHand, MediaPipeFace
 from utils_joint_angle import GestureRecognition
 import socket
-import time
-from datetime import datetime
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -17,27 +12,22 @@ mp_drawing_styles = mp.solutions.drawing_styles
 HOST = '127.0.0.1'
 PORT = 8000
 
-
 class HandRecognition:
     def __init__(self):
 
-        self.hands = MediaPipeHand(static_image_mode=False, max_num_hands=2)
-        self.disp = DisplayHand(max_num_hands=2)
+        self.hands = MediaPipeHand(static_image_mode=False, max_num_hands=1)
+        self.disp = DisplayHand(max_num_hands=1)
         self.cap = cv2.VideoCapture(0)
         self.gest = GestureRecognition(mode='eval')
         self.face_detection = MediaPipeFace(static_image_mode=False)
         self.client_socket = None
 
         self.img = self.cap.read()
-        self.before_gesture = ""
-        self.inner = False
-        self.count_enable = True
-        self.token = False
         self.counter = 0
-        self.pose = None
+        self.craneDown = False
         self.direction = 'out'
-        self.direction_before = 'out'
-        # 45 = camera fps(15fps) * 4 (sec)
+        self.res = ["", ""]
+        # 60 = camera fps(15fps) * 4 (sec)
         self.time_limit = 60
 
     def socketServer(self, host, port):
@@ -63,6 +53,7 @@ class HandRecognition:
                 0 < np.dot(np.cross(c - rt, q - rt), np.cross(q - rt, lt - rt)) and
                 0 < np.dot(np.cross(lt - c, q - c), np.cross(q - c, rt - c))):
             self.direction = "up"
+
         elif (0 < np.dot(np.cross(lt - lb, q - lb), np.cross(q - lb, lb - lt)) and
               0 < np.dot(np.cross(c - lt, q - lt), np.cross(q - lt, lb - lt)) and
               0 < np.dot(np.cross(lb - c, q - c), np.cross(q - c, lt - c))):
@@ -78,6 +69,7 @@ class HandRecognition:
               0 < np.dot(np.cross(rt - c, q - c), np.cross(q - c, rb - c))):
             self.direction = "right"
         self.img = cv2.putText(self.img, self.direction, (center[0], 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+        self.res[0] = self.direction
         return self.direction
 
     def run(self):
@@ -87,27 +79,7 @@ class HandRecognition:
             if not ret:
                 break
 
-            # Flip image for 3rd person view
-            self.img = cv2.flip(self.img, 1)
-
-            # To improve performance, optionally mark image as not writeable to pass by reference
-            self.img.flags.writeable = False
-
-            # Feedforward to extract keypoint
-            param = self.hands.forward(self.img)
-            # self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
-            evaluate = self.gest.eval(param[0]['angle'])
-            if evaluate != self.pose:
-                self.token = True
-            else:
-                self.token = False
-
-            self.pose = evaluate
-            if param[0]['class'] is not None:
-                param[0]['gesture'] = self.gest.eval(param[0]['angle'])
-
             # UI
-
             rec_size = (180, 180)
             rec_center = (int(self.img.shape[1] / 2), int(self.img.shape[0] / 2))
             rec_left_top = ((rec_center[0] - rec_size[0]), (rec_center[1] - rec_size[1]))
@@ -120,56 +92,64 @@ class HandRecognition:
             self.img = cv2.line(self.img, (rec_bot_right[0], rec_left_top[1]), (rec_left_top[0], rec_bot_right[1]),
                                 (255, 0, 0), 2)
 
+            # Flip image for 3rd person view
+            self.img = cv2.flip(self.img, 1)
+
+            # To improve performance, optionally mark image as not writeable to pass by reference
+            self.img.flags.writeable = False
+
+            # Feedforward to extract keypoint
+            param = self.hands.forward(self.img)
+            # self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+            evaluate = self.gest.eval(param[0]['angle'])
+            if param[0]['class'] is not None:
+                param[0]['gesture'] = self.gest.eval(param[0]['angle'])
+
             # evaluate
             finger_tip = param[0]['keypt'][8]
 
-            if self.pose == 'one':
-
-                # if rec_bot_right[0] > finger_tip[0] > rec_left_top[0] \
-                #         and rec_bot_right[1] > finger_tip[1] > rec_left_top[1]:
-                #     self.inner = True
-                # else:
-                #     self.inner = False
+            if evaluate == 'one':
                 self.isInside(rec_size, rec_center, finger_tip)
-                if self.direction != self.direction_before:
-                    self.token = True
-                else:
-                    self.token = False
+                self.counter = 0
+                self.craneDown = False
 
-            elif self.pose == 'fist':
+            elif evaluate == 'fist':
                 hand_center = param[0]['keypt'][9]
+
+                if not self.craneDown:
+                    self.res[0] = "out"
+
                 if rec_bot_right[0] > hand_center[0] > rec_left_top[0] \
                         and rec_bot_right[1] > hand_center[1] > rec_left_top[1]:
 
                     self.counter += 1
-                    self.img = cv2.putText(self.img, str(int(self.counter*100 / self.time_limit))+"%", (rec_center[0] - 50, 200),
-                                           cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
                     if self.counter > self.time_limit:
-                        self.token = True
+                        self.res[0] = "crain down"
+                        self.craneDown = True
+
+                    if self.counter <= self.time_limit and not self.craneDown:
+                        self.img = cv2.putText(self.img, str(int(self.counter * 100 / self.time_limit)) + "%",
+                                               (rec_center[0] - 50, 200),
+                                               cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                    elif self.craneDown:
                         self.img = cv2.putText(self.img, "crain down", (rec_center[0] - 250, 200),
                                                cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 2)
-                    else:
-                        self.token = False
+                        self.counter = 0
 
-                else:
-                    self.counter = 0
             else:
+                self.res[0] = "out"
                 self.counter = 0
+                self.craneDown = False
 
-            if self.token:
-                if self.pose == 'one':
-                    self.client_socket.sendall(self.direction.encode())
-                    print(self.direction)
-                elif self.pose == 'fist':
-                    self.client_socket.sendall("crane".encode())
-                    print("crane down")
-                    self.counter = 0
+            if self.res[0] != self.res[1]:
+                self.res[1] = self.res[0]
+                self.counter = 0
+                print(self.res[0])
+                self.client_socket.sendall(self.res[0].encode())
 
-            self.direction_before = self.direction
             self.img.flags.writeable = True
 
             # Display keypoint
-
             cv2.imshow('self.img 2D', self.disp.draw2d(self.img.copy(), param))
 
             key = cv2.waitKey(1)
